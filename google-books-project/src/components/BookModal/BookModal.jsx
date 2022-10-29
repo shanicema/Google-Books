da442cc6edf609b295cd3f4d75438e05b3f993de
Shanice <mashnce@gmail.com>
2022-10-29 23:57:36 +1100

    added styling for modal

--- google-books-project/src/components/BookModal/BookModal.jsx
+++ google-books-project/src/components/BookModal/BookModal.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import ModalContext from "../ModalContext/ModalContext";
+import style from './BookModal.module.scss';
 
 const BookModal = () => {
 
@@ -12,10 +13,24 @@ const BookModal = () => {
     }
 
     return (
-        <section style={{ position: 'fixed', top: '50%', backgroundColor: 'black'}}>
-            <p>{book.title}</p>
-            <button onClick={() => setBook(undefined)}>close</button>
-        </section>
+        <div className={style.Modal}>
+            <section className={style.Modal__Container}>
+                <div className={style.Modal__Container__ImgContainer}>
+                    <img className={style.Modal__Container__Image} src ={`${book.imageLinks?.thumbnail || 'src/assets/no-image.png' }`} />
+                </div>
+                <p>{`Title: ${book.title}`}</p>
+                <p>{`Author(s): ${book.authors?.join(', ') || "Anonymous"}`}</p>
+                <p className={style.Modal__Container__Desc}>{`Book Description: ${book.description}`}</p>
+                <p>{`Pages: ${book.pageCount}`}</p>
+                {/* <p>{`Subtitle: ${book.subtitle || "N/A"}`}</p> */}
+                <p>{`Publisher: ${book.publisher || "Unknown"}`}</p>
+                <p>{`Published Date: ${book.publishedDate}`}</p>
+                <div className={style.Modal__Container__BtnContainer}>
+                    <button className={style.Modal__Container__BtnContainer__Close} onClick={() => setBook(undefined)}>Close</button>
+                </div>
+               
+            </section>
+        </div>
     );
 };
 
