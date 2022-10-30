37f61ac4aba71a1bf7f9b16b2f233127f244cd9a
Shanice <mashnce@gmail.com>
2022-10-30 22:21:19 +1100

    added extra styling for modal

--- google-books-project/src/components/BookModal/BookModal.jsx
+++ google-books-project/src/components/BookModal/BookModal.jsx
@@ -18,13 +18,12 @@ const BookModal = () => {
                 <div className={style.Modal__Container__ImgContainer}>
                     <img className={style.Modal__Container__Image} src ={`${book.imageLinks?.thumbnail || 'src/assets/no-image.png' }`} />
                 </div>
-                <p>{`Title: ${book.title}`}</p>
-                <p>{`Author(s): ${book.authors?.join(', ') || "Anonymous"}`}</p>
-                <p className={style.Modal__Container__Desc}>{`Book Description: ${book.description}`}</p>
-                <p>{`Pages: ${book.pageCount}`}</p>
-                {/* <p>{`Subtitle: ${book.subtitle || "N/A"}`}</p> */}
-                <p>{`Publisher: ${book.publisher || "Unknown"}`}</p>
-                <p>{`Published Date: ${book.publishedDate}`}</p>
+                <p><span className={style.Modal__Bold}>Title:</span> {book.title}</p>
+                <p><span className={style.Modal__Bold}>Authors:</span> {book.authors?.join(', ') || "Anonymous"}</p>
+                <p className={style.Modal__Container__Desc}><span className={style.Modal__Bold}>Book Description:</span> {book.description}</p>
+                <p><span className={style.Modal__Bold}>Pages:</span> {book.pageCount}</p>
+                <p><span className={style.Modal__Bold}>Publisher:</span> {book.publisher || "Unknown"}</p>
+                <p><span className={style.Modal__Bold}>Published Date:</span> {book.publishedDate}</p>
                 <div className={style.Modal__Container__BtnContainer}>
                     <button className={style.Modal__Container__BtnContainer__Close} onClick={() => setBook(undefined)}>Close</button>
                 </div>
